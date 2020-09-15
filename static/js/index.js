23769ec5766b7a784f762b8544a3ea444ff71a89
Julio <jn5.guape@gmail.com>
2020-09-15 00:50:36 -0300

    volte ao topo e menu

---
+++ static/js/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    // Exibe ou oculta o botão
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+            $('.volteaotopo').fadeIn(200);
+        } else {
+            $('.volteaotopo').fadeOut(200);
+        }
+    });
+
+    // Faz animação para subir
+    $('.volteaotopo').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, 300);
+    })
+});
